e2b56039d459632563bc7fdb07f008534c4d1907
axelinter <axel.telkamper@gmail.com>
2017-12-04 20:29:19 +0100

    Actualizacion
    Auto mejorado

--- tictactoe/src/tictactoe/Tictactoe.java
+++ tictactoe/src/tictactoe/Tictactoe.java
@@ -37,7 +37,7 @@ public class Tictactoe {
                     case '1':
                         mi_juego.inicializa();
                     mi_juego.dibuja_tres_en_raya();
-                    mi_juego.comenzar_a_jugar();
+                    mi_juego.comenzar_a_jugar_auto();
                         break;
                      
                     case '2':
@@ -128,12 +128,13 @@ public void comenzar_a_jugar(){
             }
             if(valor_juego==0){
                 System.out.println("Te toca 0");
+                
             }
-            System.out.println("Introduce coordenadas de posición: (0,0) ");
+           System.out.println("Introduce coordenadas de posición: (0,0) ");
             System.out.println("Introduce la fila");
             int fila= Integer.parseInt(teclado.readLine());
             System.out.println("Introduce la columna");
-            int columna= Integer.parseInt(teclado.readLine());
+            int columna= Integer.parseInt(teclado.readLine()); 
             if(tablero[fila][columna]==3){
                 boolean ganador;
                 tablero[fila][columna]=valor_juego;
@@ -169,6 +170,128 @@ public void comenzar_a_jugar(){
             }else{
                 System.out.println("No puede jugar, ya está usado");
             }
+            }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Tictactoe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+}
+public void comenzar_a_jugar_auto(){
+        try {
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            String introduce0;
+            String introduceX;
+            boolean fin=false;
+           
+            System.out.println("¿Cómo quieres jugar X/0?");
+            String juego = teclado.readLine().toUpperCase();
+            int valor_juego=0;
+            if(juego.equals("0")){
+                valor_juego=0;
+            }
+            if(juego.equals("X")){
+                
+                valor_juego=1;
+                
+            }
+            if(juego.equals("X") || (juego.equals("0"))){
+            
+            System.out.println("Comienza el 0");
+            
+            System.out.println("");
+            
+            while(fin==false){
+            dibuja_tres_en_raya();
+            if(valor_juego==1){
+                System.out.println("Te toca X");
+                System.out.println("Introduce coordenadas de posición: (0,0) ");
+            System.out.println("Introduce la fila");
+            int fila= Integer.parseInt(teclado.readLine());
+            System.out.println("Introduce la columna");
+            int columna= Integer.parseInt(teclado.readLine()); 
+            if(tablero[fila][columna]==3){
+                boolean ganador;
+                tablero[fila][columna]=valor_juego;
+                dibuja_tres_en_raya();
+                ganador=comprobar_ganador(valor_juego);
+                
+                if(ganador){
+                    System.out.println("Ha ganado " + valor_juego);
+                    fin=true;
+                    inicializa();
+                }else {
+                    int estado_empate=0;
+                    for(int i=0;i<=2;i++){
+                        for(int j=0;j<=2;j++){
+                            if((tablero[i][j]==0) || (tablero[i][j]==1)){
+                                estado_empate=estado_empate+1;
+                            }
+                        }
+                    }
+                    if(estado_empate==9){
+                        System.out.println("Se ha empatado");
+                        fin=true;
+                    }
+                    if(valor_juego==1){
+                        valor_juego=0;
+                    }
+                    else{
+                        if(valor_juego==0){
+                        valor_juego=1;
+                        }
+                    }
+                }
+            }else{
+                System.out.println("No puede jugar, ya está usado");
+            }
+            }
+            if(valor_juego==0){
+                System.out.println("Te toca 0");
+                int numeroAleatorio = (int) (Math.random()*2+0);
+            
+            int fila= numeroAleatorio;
+            numeroAleatorio = (int) (Math.random()*2+0);
+            int columna = numeroAleatorio;
+            
+            if(tablero[fila][columna]==3){
+                boolean ganador;
+                tablero[fila][columna]=valor_juego;
+                dibuja_tres_en_raya();
+                ganador=comprobar_ganador(valor_juego);
+                
+                if(ganador){
+                    System.out.println("Ha ganado " + valor_juego);
+                    fin=true;
+                    inicializa();
+                }else {
+                    int estado_empate=0;
+                    for(int i=0;i<=2;i++){
+                        for(int j=0;j<=2;j++){
+                            if((tablero[i][j]==0) || (tablero[i][j]==1)){
+                                estado_empate=estado_empate+1;
+                            }
+                        }
+                    }
+                    if(estado_empate==9){
+                        System.out.println("Se ha empatado");
+                        fin=true;
+                    }
+                    if(valor_juego==1){
+                        valor_juego=0;
+                    }
+                    else{
+                        if(valor_juego==0){
+                        valor_juego=1;
+                        }
+                    }
+                }
+            }else{
+                System.out.println("No puede jugar, ya está usado");
+            }
+                
+            }
+           
             }
             }
         } catch (IOException ex) {
